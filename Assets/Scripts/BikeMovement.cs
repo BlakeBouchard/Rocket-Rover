1638b35235dd15935f0f7b20cc083901e3db5577
Blake Bouchard <thatsblake@gmail.com>
2014-04-25 22:08:06 -0600

    Bike now goes vroom

---
+++ Assets/Scripts/BikeMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeMovement : MonoBehaviour {
+
+    public float acceleration = 80.0f;
+    public float jumpForce = 800.0f;
+
+    private bool onGround = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            onGround = true;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rigidbody2D.AddForce(new Vector2(Input.GetAxis("Horizontal") * acceleration, 0));
+
+        // Jump control
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            // Add upward force
+            rigidbody2D.AddForce(new Vector2(0, jumpForce));
+            onGround = false;
+        }
+	}
+}
---
+++ Assets/Scripts/WheelRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelRotation : MonoBehaviour {
+
+    public float rotationSpeed = 1.0f;
+
+    // Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0, 0, rotationSpeed);
+	}
+}
