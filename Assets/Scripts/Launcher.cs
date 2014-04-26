e71a91979f9131bfde353e2c9d9be7bf360ec399
Blake Bouchard <thatsblake@gmail.com>
2014-04-25 22:52:56 -0600

    Launcher rotation

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -12,6 +12,16 @@ public class Launcher : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-	    
+        if (Input.mousePresent)
+        {
+            Vector2 convertedMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            float mouseOffsetX = convertedMousePosition.x - transform.position.x;
+            float mouseOffsetY = convertedMousePosition.y - transform.position.y;
+
+            float angleToMouse = Mathf.Atan2(mouseOffsetY, mouseOffsetX);
+            Debug.Log("Angle to Mouse: " + angleToMouse);
+            transform.eulerAngles = new Vector3(0, 0, Mathf.Rad2Deg * angleToMouse);
+        }
 	}
 }
