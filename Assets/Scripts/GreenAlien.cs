2e1c884298c9918fa3cd8c8bdc7674219e17a0b8
Blake Bouchard <thatsblake@gmail.com>
2014-04-26 09:54:14 -0600

    Alien and Rockets don't collide with Player

--- Assets/Scripts/GreenAlien.cs
+++ Assets/Scripts/GreenAlien.cs
@@ -1,29 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Rocket : MonoBehaviour {
+public class GreenAlien : MonoBehaviour {
 
 	// Use this for initialization
 	void Start ()
     {
-	    
+	
 	}
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Boundary")
+        if (collision.collider.tag == "PlayerProjectile")
         {
             Destroy(this.gameObject);
         }
-        else if (collision.collider.tag == "Enemy")
-        {
-            Explode();
-        }
-    }
-
-    private void Explode()
-    {
-        Destroy(this.gameObject);
     }
 	
 	// Update is called once per frame
--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -9,7 +9,7 @@ public class Launcher : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-	
+        Physics2D.IgnoreLayerCollision(8, 9);
 	}
 	
 	// Update is called once per frame
