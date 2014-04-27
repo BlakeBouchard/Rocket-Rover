73d242b192fdbcc6b93d7a446e7d9857557e37b4
Blake Bouchard <thatsblake@gmail.com>
2014-04-26 22:46:07 -0600

    GUI for distance travelled

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+    public float distanceTravelled = 0;
+    GUIText distanceLabel;
+
+    public float velocity = 40.0f;
+
 	// Use this for initialization
 	void Start ()
     {
         SetUpCollisionIgnores();
+        this.distanceLabel = GameObject.Find("Distance Travelled").GetComponent<GUIText>();
 	}
 
     private void SetUpCollisionIgnores()
@@ -19,6 +25,7 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-	
+        distanceTravelled += velocity * Time.deltaTime;
+        distanceLabel.text = "Distance Travelled: " + (int)distanceTravelled + " m";
 	}
 }
